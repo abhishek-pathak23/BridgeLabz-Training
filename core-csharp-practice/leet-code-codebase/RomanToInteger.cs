ed47fbbfe1d27502d0d6f7de2091deb4dd76deb1
Abhishek Pathak <abhipathak4546@gmail.com>
2025-12-29 16:25:50 +0530

    [Abhishek Pathak] leetcode codes added

---
+++ core-csharp-practice/leet-code-codebase/RomanToInteger.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    int Value(char c) {
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+    public int RomanToInt(string s) {
+        int total = 0;
+
+        for (int i = 0; i < s.Length; i++) {
+            int current = Value(s[i]);
+            int next = (i < s.Length - 1) ? Value(s[i + 1]) : 0;
+
+            if (current < next)
+                total -= current;
+            else
+                total += current;
+        }
+
+        return total;
+    }
+}
