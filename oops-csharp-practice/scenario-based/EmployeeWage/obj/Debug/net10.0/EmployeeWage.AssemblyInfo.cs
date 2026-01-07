24c8e4b6a484d03edcd78466fe39592cea54a682
Abhishek Pathak <abhipathak4546@gmail.com>
2026-01-07 16:13:57 +0530

    [Abhishek Pathak] solved, uc-3 employee based

--- oops-csharp-practice/scenario-based/EmployeeWage/obj/Debug/net10.0/EmployeeWage.AssemblyInfo.cs
+++ oops-csharp-practice/scenario-based/EmployeeWage/obj/Debug/net10.0/EmployeeWage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeWage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972f27300d784d74290660d5d0982863277f276e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d102d04be08fa985b8eb5c7ff0cd2f8f822a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeWage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeWage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
