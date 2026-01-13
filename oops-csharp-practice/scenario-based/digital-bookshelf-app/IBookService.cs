c2046a237472e0ebb8585c8afce6917e3269d102
Abhishek Pathak <abhipathak4546@gmail.com>
2026-01-13 15:03:26 +0530

    [Abhishek Pathak] two scenario based solved

---
+++ oops-csharp-practice/scenario-based/digital-bookshelf-app/IBookService.cs
@@ -0,0 +1,8 @@
+﻿// Interface defines the allowed operations
+public interface IBookService
+{
+    void AddBook(string title, string author);
+    void SortBooksAlphabetically();
+    void SearchByAuthor(string author);
+    void ExportBooks();
+}
