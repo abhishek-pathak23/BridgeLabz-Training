87d7d33aec0756c145e0174ae56d0b9076ada2eb
Abhishek Pathak <abhipathak4546@gmail.com>
2026-01-15 16:27:32 +0530

    [Abhishek Pathak]updating

--- oops-csharp-practice/scenario-based/EmployeeWage/obj/Debug/net10.0/EmployeeWage.AssemblyInfo.cs
+++ oops-csharp-practice/scenario-based/EmployeeWage/obj/Debug/net10.0/EmployeeWage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeWage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+416adc8d85affef14739035f10260b0cb38d9abd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56441b4d72a21b69afe4e963867eeac74e6118ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeWage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeWage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
