b7587cf8ad5b43f9c5f3fae024803d65e5f1f5e9
Abhishek Pathak <abhipathak4546@gmail.com>
2026-01-19 16:22:44 +0530

    [Abhishek Pathak] Binary Tree Inorder Traversal of leetcode

---
+++ datastructure-csharp-practice/leet-code-codebase/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public IList<int> InorderTraversal(TreeNode root) {
+        List<int> result = new List<int>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode current = root;
+
+        while (current != null || stack.Count > 0) {
+            while (current != null) {
+                stack.Push(current);
+                current = current.left;
+            }
+
+            current = stack.Pop();
+            result.Add(current.val);
+            current = current.right;
+        }
+
+        return result;
+    }
+}
