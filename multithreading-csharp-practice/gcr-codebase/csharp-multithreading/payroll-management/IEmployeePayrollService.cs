94de506570862c297a2f09499ff99da60e01a987
Abhishek Pathak <abhipathak4546@gmail.com>
2026-02-04 16:35:32 +0530

    [Abhishek Pathak]Payroll Management System Solved

---
+++ multithreading-csharp-practice/gcr-codebase/csharp-multithreading/payroll-management/IEmployeePayrollService.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+namespace PayrollService
+{
+    // Interface for Employee Payroll Service
+    public interface IEmployeePayrollService
+    {
+        void AddEmployee(Employee employee);                     // UC1 & UC2
+        void AddEmployeesWithThreads(List<Employee> employees);  // UC2, UC3, UC4
+        void AddEmployeeWithDetails(Employee employee);          // UC5
+        void UpdateEmployeeSalary(Employee employee);            // UC6
+    }
+}
