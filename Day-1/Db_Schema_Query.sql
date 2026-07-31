40b30a55b14570997fedd13945e4b3bd28ea07c4
Abhishek Pathak <abhipathak4546@gmail.com>
2026-07-31 15:54:48 +0530

    [Abhishek Pathak] Improvised constraints in the query

--- Day-1/Db_Schema_Query.sql
+++ Day-1/Db_Schema_Query.sql
@@ -1,26 +1,32 @@
-use healthappdb;
+USE healthappdb;
+GO
+
 CREATE TABLE Patients
 (
     PatientID INT PRIMARY KEY IDENTITY(1,1),
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50),
     DateOfBirth DATE,
-    Gender VARCHAR(10),
-    Phone VARCHAR(15)
+    Gender VARCHAR(10)
+        CHECK (Gender IN ('Male', 'Female', 'Other')),
+    Phone VARCHAR(15) UNIQUE
 );
 
-select * from Patients
+SELECT * FROM Patients;
+
 
 CREATE TABLE Doctors
 (
     DoctorID INT PRIMARY KEY IDENTITY(1,1),
     DoctorName VARCHAR(100) NOT NULL,
     Specialization VARCHAR(100),
-    Phone VARCHAR(15),
+    Phone VARCHAR(15) UNIQUE,
     ExperienceYears INT
+        CHECK (ExperienceYears >= 0)
 );
 
-select * from Doctors
+SELECT * FROM Doctors;
+
 
 CREATE TABLE Appointments
 (
@@ -30,13 +36,19 @@ CREATE TABLE Appointments
     DoctorID INT NOT NULL,
 
     AppointmentDate DATETIME NOT NULL,
-    Status VARCHAR(20),
+
+    Status VARCHAR(20)
+        DEFAULT 'Scheduled'
+        CHECK (Status IN ('Scheduled', 'Completed', 'Cancelled')),
 
     FOREIGN KEY (PatientID)
-    REFERENCES Patients(PatientID),
+        REFERENCES Patients(PatientID),
 
     FOREIGN KEY (DoctorID)
-    REFERENCES Doctors(DoctorID)
+        REFERENCES Doctors(DoctorID)
 );
 
-select * from Appointments
+SELECT * FROM Appointments;
+
+
+
